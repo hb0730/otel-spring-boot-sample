fc0602ed29d0a6d68152b69143e7764eb7d48cbb
hb0730 <1278032416@qq.com>
2024-04-20 23:17:54 +0800

    feat: init

---
+++ src/main/java/com/hb0730/otel/sample/OtelSpringBootSampleApplication.java
@@ -0,0 +1,15 @@
+package com.hb0730.otel.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:huangbing0730@gmail">hb0730</a>
+ * @date 2024/4/20
+ */
+@SpringBootApplication
+public class OtelSpringBootSampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OtelSpringBootSampleApplication.class, args);
+    }
+}
