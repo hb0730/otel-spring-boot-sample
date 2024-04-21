f53c6797769725acdb53d092707eff6badd1eb5e
hb0730 <1278032416@qq.com>
2024-04-21 16:18:27 +0800

    fix: update

--- src/main/java/com/hb0730/otel/sample/OtelSpringBootSampleApplication.java
+++ src/main/java/com/hb0730/otel/sample/OtelSpringBootSampleApplication.java
@@ -1,15 +1,29 @@
 package com.hb0730.otel.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author <a href="mailto:huangbing0730@gmail">hb0730</a>
  * @date 2024/4/20
  */
 @SpringBootApplication
+@RestController
 public class OtelSpringBootSampleApplication {
+    private static final Logger logger = LoggerFactory.getLogger(OtelSpringBootSampleApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(OtelSpringBootSampleApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> hello() {
+        logger.info("Hello, World!");
+        return ResponseEntity.ok("Hello, World!");
     }
 }
